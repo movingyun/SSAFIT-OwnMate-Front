2f9e753046b725423bb8fb82025256fc61c0bbc7
송유경 <syg9929@naver.com>
2022-05-18 22:38:21 +0900

    중간저장

---
+++ ownmate-vue/src/components/common/HeaderNav.vue
@@ -0,0 +1,26 @@
+<template>
+  <header>
+      <nav class="header-nav">
+          <div>
+              <router-link to="#">홈</router-link>
+          </div>
+          <div>
+              <router-link to="#">탐색</router-link> <br>
+              <router-link to="#">팔로우 목록</router-link> <br>
+              <router-link to="#">찜한 영상</router-link> <br>
+          </div>
+      </nav>
+  </header>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+    .header-nav{
+        
+    }
+</style>
